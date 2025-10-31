44846cead8727e71c865fdf43919a21b1ade6a50
dsmb95 <daniellesamanthamananquil@gmail.com>
2025-10-30 17:09:01 -0700

    Completed Instructions
    Completed instructions on lesson 8.

--- script.js
+++ script.js
@@ -14,22 +14,24 @@ const weatherData = {
 
 // ----- Function to update DOM -----
 function displayWeather(data) {
-  document.getElementById("cityName").textContent = "Your city";
-  document.getElementById("temperature").textContent = `8°C`;
-  document.getElementById("feelslike").textContent = `6°C`;
-  document.getElementById("humidity").textContent = `90%`;
-  document.getElementById("weatherDesc").textContent = `Moderate rain`;
-  document.getElementById("weatherIcon").src = "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0010_heavy_rain_showers.png";
+  document.getElementById("cityName").textContent = data.location.name;
+  document.getElementById("temperature").textContent = data.current.temperature;
+  document.getElementById("feelslike").textContent = data.current.feelslike;
+  document.getElementById("humidity").textContent = `${data.current.humidity}%`;
+  document.getElementById("weatherDesc").textContent = data.current.weather_descriptions;
+  document.getElementById("weatherIcon").src = data.current.weather_icons[0];
 }
 
 
 // ----- Modern way: Fetch API -----
-async function getWeather() {
-  const city = document.getElementById("cityInput").value || "Mississauga";
+function getWeather() {
+  const city = document.getElementById("cityInput").value || "Vancouver";
   const errorMsg = document.getElementById("errorMsg");
   try {
-    displayData={};
-    displayWeather(displayData);
+    fetch(`https://api.weatherstack.com/current?access_key=e1dcffa01f3b13bf3bd96c52dd21a6eb&query=${city}`)//send a request
+      .then(response => response.json()) //we receive a message from API and unwrap the envelope and give us a JS object using .json()
+      .then(data => displayWeather(data)) //the package from the API that is stored in 'response' will be thrown to 'data' where we perform an action on it
+      .catch(error => console.log(error)); // We used chaining.
   } catch (error) {
     console.error(error);
     errorMsg.classList.remove("hidden");
